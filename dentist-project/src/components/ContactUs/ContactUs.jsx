5597f0a9166da49ba2142253ec59ea08d6c60ebe
Emanuel Mhlanga <Emanuelmhlanga55@gmail.com>
2024-12-11 15:07:54 +0300

    Contact Form Now Working!

--- dentist-project/src/components/ContactUs/ContactUs.jsx
+++ dentist-project/src/components/ContactUs/ContactUs.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 import { FaRegBuilding } from "react-icons/fa";
 import { BsTelephone } from "react-icons/bs";
 import { CiMail } from "react-icons/ci";
+import Swal from 'sweetalert2';
 
 const ContactUs = () => {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        
+        // Got the Key On My Email
+        formData.append("access_key", "66cdec58-2096-4c27-8230-09f973deef19");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+            Swal.fire({
+                title: "Done!",
+                text: "Message Succesfully Sent!",
+                icon: "success"
+              });
+        }
+    }
+
   return (
     <>
     <div>
@@ -38,27 +68,42 @@ const ContactUs = () => {
                 
             </div>
 
+
+
             </div>
             {/* Contact Form Section */}
             <div className='pt-5'>
-                <div className=' rounded-md flex flex-col gap-3 m-1 h-full'>
-                    {/* First Name & Last Name */}
-                    <div className='flex flex-row gap-2 ml-5 mr-5'>
-                        <input type="text" placeholder='FirstName' className='border border-gray-300 rounded-lg p-2 w-full focus:outline-none focus:ring-1 focus:ring-primary' />
-                        <input type="text" placeholder='LastName' className='border border-gray-300 rounded-lg p-2 w-full focus:outline-none focus:ring-1 focus:ring-primary' />
-                    </div>
+        <form onSubmit={onSubmit}>
+            <div className=' rounded-md flex flex-col gap-3 m-1 h-full'>
 
-                    <input type="email" placeholder='Email' className='border border-gray-300 rounded-lg p-2 ml-5 mr-5  focus:outline-none focus:ring-1 focus:ring-primary ' />
-                    
-                    <input type="number" placeholder='Phone Number' className='border border-gray-300 rounded-lg p-2 ml-5 mr-5  focus:outline-none focus:ring-1 focus:ring-primary ' />
-
-                    <input type="text" placeholder='Message' className='border border-gray-300 rounded-lg p-2 ml-5 mr-5 h-[150px] focus:outline-none focus:ring-1 focus:ring-primary ' />
-
-                    {/* Send Message Button */}
-                    <div className='self-center w-full pr-5 pl-5'>
-                        <button className='btn-primary w-full'>Send Message</button>
-                    </div>
+                {/* First Name & Last Name */}
+                <div className='flex flex-row gap-2 ml-5 mr-5'>
+                    <input type="text" placeholder='FirstName' required name='firstname'
+                    className='border border-gray-300 rounded-lg p-2 w-full focus:outline-none focus:ring-1 focus:ring-primary' />
+                    <input type="text" placeholder='LastName' required name='lastname'
+                    className='border border-gray-300 rounded-lg p-2 w-full focus:outline-none focus:ring-1 focus:ring-primary' />
                 </div>
+
+                {/* Email */}
+                <input type="email" placeholder='Email' required name='email '
+                className='border border-gray-300 rounded-lg p-2 ml-5 mr-5  focus:outline-none focus:ring-1 focus:ring-primary ' />
+
+                {/* Phone Number */}
+                <input type="number" placeholder='Phone Number' required name='phone'
+                className='border border-gray-300 rounded-lg p-2 ml-5 mr-5  focus:outline-none focus:ring-1 focus:ring-primary ' />
+
+                {/* Message */}
+                <input type="text" placeholder='Message' required name='message'
+                className='border border-gray-300 rounded-lg p-2 ml-5 mr-5 h-[150px] focus:outline-none focus:ring-1 focus:ring-primary ' />
+
+                {/* Send Message Button */}
+                <div className='self-center w-full pr-5 pl-5'>
+                    <button className='btn-primary w-full'>Send Message</button>
+                </div>
+            </div>
+
+        </form>
+               
             </div>
 
                 
